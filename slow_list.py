edb7278949f74b5eef12d89bfb65babc5214f2e3
RDornodon <archelf.hun@gmail.com>
2020-12-30 12:46:46 +0100

    slowlist added

---
+++ slow_list.py
@@ -0,0 +1,24 @@
+import time
+
+class slowlist(list):
+    def __next__(self):
+        time.sleep(0.01)
+        return self.__iter.__next__()
+
+    def __getitem__(self, item):
+        time.sleep(0.01)
+        return super().__getitem__(item)
+
+    def index(self, __value, __start = ..., __stop = ...):
+        raise Exception("Do not use index!")
+
+    def __iter__(self):
+        self.__iter = super().__iter__()
+        return self
+
+
+t = time.time()
+l = slowlist(range(100))
+L = [*l]
+print(l[3],l[5],l[7],l[11],l[44])
+print(f'{time.time()-t:.2f} secs elapsed')
