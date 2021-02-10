aa497e92571de55948f31057b79c3d188394f014
RDornodon <archelf.hun@gmail.com>
2021-02-10 18:19:48 +0100

    40.2 revised (to return -1 on no meetings)

--- runtime_error.py
+++ runtime_error.py
@@ -1,6 +1,6 @@
 def solution(s, v):
     Z = [*zip(range(len(s)), s, v)]
-    T = []
+    T = [0]
     C = 0
     for x, s, v in Z:
         for X, S, V in Z:
@@ -15,6 +15,7 @@ def solution(s, v):
 
 solution=lambda s,v:(Z:=[*zip(range(len(s)),*zip(*{*zip(s,v)}))])and(Q:=[(S-s)/(v-V)if(v-V)else-1for x,s,v in Z for X,S,V in Z if x>X])and max(Q.count(q)for q in{*Q}if 0<=q)+len(s)-len(Z)
 L,W,solution=len,zip,lambda s,v:(k:=((Z:=[*W(range(L(s)),*W(*{*W(s,v)}))])and(Q:=[(S-s)/(v-V)if(v-V)else-1for x,s,v in Z for X,S,V in Z if x>X]+[0])and max([Q.count(q)for q in{*Q}if 0<=q])+L(s)-L(Z)))*[-1,1][k>1]
+L,W,solution=len,zip,lambda s,v:[k:=((Z:=[*W(range(L(s)),*W(*{*W(s,v)}))])and(Q:=[(S-s)/(v-V)if(v-V)else-1for x,s,v in Z for X,S,V in Z if x>X]+[0])and max([Q.count(q)for q in{*Q}if 0<=q])+L(s)-L(Z)),-k][k<2]
 
 
 
@@ -22,5 +23,9 @@ if __name__=='__main__':
     print(solution([1, 4, 2], [27, 18, 24]))
     print(solution([1, 4, 2, 2], [27, 18, 24, 24]))
     print(solution([2, 2], [24, 24]))
+    print(solution([2, 2, 2], [24, 24, 24]))
+    print(solution([2, 2, 2], [24, 24, 25]))
     print(solution([2, 3], [24, 24]))
+    print(solution([2, 3, 4, 5, 6], [1, 2, 3, 4, 5]))
+    print(solution([2, 3, 4, 5, 6], [1, 1, 1, 1, 1]))
     print(solution([2, 2], [24, 25]))
